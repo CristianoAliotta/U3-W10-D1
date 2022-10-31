ef5db6b6e26b16a09c45d68e75b5cf31beb06f1d
CristianoAliotta <cristiano.aliotta@outlook.com>
2022-10-31 17:05:24 +0100

    exercise completed

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -2,10 +2,24 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { LogoComponent } from './logo/logo.component';
+import { NavComponent } from './nav/nav.component';
+import { MainComponent } from './main/main.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { MainCardComponent } from './main-card/main-card.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LogoComponent,
+    NavComponent,
+    MainComponent,
+    CarouselComponent,
+    MainCardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
